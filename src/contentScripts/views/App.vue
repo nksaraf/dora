dae29609f284a74d21d18e4682b28cda8ea0716e
Nikhil Saraf <nsaraf@purestorage.com>
2022-03-06 22:39:36 -0800

    add terminal

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,20 +1,14 @@
 <template>
-  <div class="fixed right-0 bottom-0 m-5 z-100 flex font-sans select-none leading-1em">
-    <div
-      class="bg-white text-gray-800 rounded-full shadow w-max h-min"
-      p="x-4 y-2"
-      m="y-auto r-2"
-      transition="opacity duration-300"
-      :class="show ? 'opacity-100' : 'opacity-0'"
-    >
-      Vitesse WebExt
-    </div>
-    <div
-      class="flex w-10 h-10 rounded-full shadow cursor-pointer"
-      bg="teal-600 hover:teal-700"
+  <div class="fixed right-0 bottom-10 my-5 z-100 flex font-sans select-none leading-1em">
+    <div v-if="show" class="px-2 py-2 bg-[#2b2b2b] rounded-l-md relative"><button @click="toggle()" class="absolute bg-transparent border-none right-1 top-1 cursor-pointer">
+      <eva:close-fill class="block m-auto text-white text-lg bg-[#2b2b2b]"  />
+      </button><iframe src="http://localhost:7681" class="px-2 border-none w-120 h-64" /></div>
+    <div v-else
+      class="flex w-10 h-12 rounded-l-md shadow cursor-pointer"
+      bg="[#2b2b2b] hover:teal-700"
       @click="toggle()"
     >
-      <pixelarticons-power class="block m-auto text-white text-lg" />
+      <fa-solid:terminal class="block m-auto text-white text-lg" />
     </div>
   </div>
 </template>
@@ -22,6 +16,23 @@
 <script setup lang="ts">
 import { useToggle } from '@vueuse/core'
 import 'virtual:windi.css'
-
+import hotkeys from 'hotkeys-js'
+import clipboard from 'clipboardy';
 const [show, toggle] = useToggle(false)
+
+hotkeys('ctrl+`', function(event, handler) {
+  event.preventDefault();
+  
+  (async () => {
+    await clipboard.write(window.location.href);
+
+    // Prevent the default refresh event under WINDOWS system
+    if (!show.value) {
+      toggle()
+    }
+  })()
+
+});
+
+
 </script>
